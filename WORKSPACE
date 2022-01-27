6d78d491552db6873bb5decf516fdc88481fc047
nmelihsensoy <nmelihsensoy@gmail.com>
2022-01-27 22:30:28 +0300

    Added automated build and release

--- WORKSPACE
+++ WORKSPACE
@@ -1,9 +1,6 @@
-# external dependencies
-
 # android ndk
 # currently only ndk version r21 or below is supported from bazel. any ndks newer than version r21 doesn't have `platforms` folder and that cause the following error.
 # "build aborted: Expected directory at <NDKPATH>/platforms but it is not a directory or it does not exist."
-
 android_ndk_repository(
     name="androidndk",
     #path = "<NDKPATH>", #Uncomment this line to use specific ndk. Otherwise `ANDROID_NDK_HOME` is used.
---
+++ release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+## 
+## Release script for 'android-blockevent' application. 
+## Builds the application executables for multiple platforms into 'releases' directory.
+## Can take version number as an argument. It's optional.
+## Valid version numbers: 1.0, v1.0, 1.0.0, v1.0.0, 2.9.9, v99.9.9
+##
+
+out_dir="releases"
+bin_dir="bazel-bin"
+apps=("src:android-blockevent")
+arch_attr="--config"
+archs=("android_arm" "android_arm64" "android_x86" "android_x86_64")
+version=""
+declare -a output_files
+declare -a default_bazel_flags=(build -c opt)
+declare -a bazel_flags
+
+if [ $# -gt 1 ]; then
+    echo "Too many arguments. Aborted." >&2
+    exit 1
+elif [ $# -eq 1 ]; then
+    [[ "$1" =~ ^v?(([0-9]{1,2})\.([0-9])(\.([0-9]))?)$ ]] && version="_${1#v}" || \
+        >&2 echo "Invalid Version: '$1'. Building versionless." 
+else
+    echo "Building versionless." 
+fi
+
+for app in ${apps[@]}; do
+    bazel_flags=("${default_bazel_flags[@]}")
+    bazel_flags+=("//"${app})
+    bazel_flags+=(${arch_attr}=)
+
+    for arch in ${archs[@]}; do
+        output_filename=${app##*:}${version}_${arch#android_}
+
+        bazel "${bazel_flags[@]}${arch}"
+        mkdir -p "${out_dir}" && cp -f "${bin_dir}/${app%%:*}/${app##*:}" "${out_dir}/${output_filename}"
+        [[ $? -eq 0 ]] || >&2 echo "Error occured when copying: '${output_filename}'"
+        echo "`sha256sum ${out_dir}/${output_filename} | cut -d " " -f1` ${output_filename}" >> \
+            "${out_dir}/checksums${version}.txt"
+        
+        output_files+=(${out_dir}/${output_filename})
+    done
+done
+
+for out in ${output_files[@]}; do
+    file $out
+done
